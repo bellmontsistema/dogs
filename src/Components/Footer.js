770a22ba4b6e737ddce0fd72b2081513a0ae563a
bellmontsistema <bellmontsistema@gmail.com>
2021-05-03 21:48:19 -0300

    aula 0806

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={Styles.footer}>
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
